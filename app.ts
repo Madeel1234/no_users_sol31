b9a7f39a261afafca73d9e3591898a551f60f365
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 18:31:55 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+
+// Task # 31
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// If the list is empty, print the message we need to find some users!
+// Remove all of the users names from your array, and make sure the correct message is printed.
+
+
+let users : string [] = ["Admin","Eric","Aiza","Fatima","Ali"]
+users = []
+if(users.length===0){
+    console.log("we need to find some users!")
+}
